bca1e1fd09c87abbac9c2455dcf01cb7379552a4
kemi-p <kpole@dvtsoftware.com>
2026-05-04 08:42:11 +0200

    new track card component, move from css to scss

---
+++ src/app/components/track-card/track-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-card',
+  imports: [],
+  templateUrl: './track-card.html',
+  styleUrl: './track-card.scss',
+})
+export class TrackCard {}
