b099b166029acf8c4f5f0094da4e42172977fb66
kemi-p <kpole@dvtsoftware.com>
2026-05-04 11:40:11 +0200

    endpoints for search tracks

--- src/app/core/services/deezerApi.ts
+++ src/app/core/services/deezerApi.ts
@@ -1,15 +1,20 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ArtistDetail, SearchResponse } from '../models/deezer';
+import { ArtistDetail, SearchResponse, Track } from '../models/deezer';
 
 @Injectable({ providedIn: 'root' })
 export class DeezerService {
-  base = '/api';
+  base = 'https://api.deezer.com';
   private http = inject(HttpClient);
 
   searchArtist(query: string): Observable<SearchResponse<ArtistDetail>> {
     const params = new HttpParams().set('q', query).set('limit', 20);
     return this.http.get<SearchResponse<ArtistDetail>>(`${this.base}/search/artist`, { params });
+  }
+
+  searchTracks(query: string): Observable<SearchResponse<Track>> {
+    const params = new HttpParams().set('q', query).set('limit', 20);
+    return this.http.get<SearchResponse<Track>>(`${this.base}/search`, { params });
   }
 }
