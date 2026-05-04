29448611493f39bc6ebb20e20c535e4ca546b43d
kemi-p <kpole@dvtsoftware.com>
2026-05-04 10:29:49 +0200

    interafces for the ablbum and artist details

--- src/app/core/models/deezer.ts
+++ src/app/core/models/deezer.ts
@@ -12,3 +12,30 @@ export interface DeezerArtist {
   tracklist?: string;
   link?: string;
 }
+
+export interface DeezerArtistDetail extends DeezerArtist {
+  nb_album: number;
+  nb_fan: number;
+  radio: boolean;
+  tracklist: string;
+  link: string;
+}
+
+export interface DeezerAlbum {
+  id: number;
+  title: string;
+  cover: string;
+  cover_small: string;
+  cover_medium: string;
+  cover_big: string;
+  cover_xl: string;
+  release_date: string;
+  tracklist?: string;
+  artist?: DeezerArtist;
+  nb_tracks?: number;
+  genre_id?: number;
+  fans?: number;
+  record_type?: string;
+  explicit_lyrics?: boolean;
+  link?: string;
+}
