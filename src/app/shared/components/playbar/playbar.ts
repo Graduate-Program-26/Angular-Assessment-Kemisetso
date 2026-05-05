efa9d42594bb417eaa92cf6881afca27317f1646
kemi-p <kpole@dvtsoftware.com>
2026-05-05 13:20:54 +0200

    feat(playbar):layout of the plabar and styling

--- src/app/shared/components/playbar/playbar.ts
+++ src/app/shared/components/playbar/playbar.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { SliderModule } from 'primeng/slider';
+import { FormsModule } from '@angular/forms';
+import { PlaybarStore } from '../../../core/stores/playbarStore';
+import { DurationPipe } from '../../pipes/duration';
 @Component({
   selector: 'app-playbar',
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [SliderModule, FormsModule, DurationPipe],
   templateUrl: './playbar.html',
   styleUrl: './playbar.scss',
 })
-export class Playbar {}
+export class Playbar {
+  player = inject(PlaybarStore);
+}
