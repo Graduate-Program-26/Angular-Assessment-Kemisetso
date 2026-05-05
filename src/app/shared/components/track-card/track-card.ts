b354e1eeaac5e5bf36383d306c7f8e871a55979d
kemi-p <kpole@dvtsoftware.com>
2026-05-05 10:33:07 +0200

    remove unused component from initial scaffolding

--- src/app/shared/components/track-card/track-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-track-card',
-  imports: [],
-  templateUrl: './track-card.html',
-  styleUrl: './track-card.scss',
-})
-export class TrackCard {}
