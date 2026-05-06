26822d643d57548b974a09388a323580e448cad5
kemi-p <kpole@dvtsoftware.com>
2026-05-06 09:58:42 +0200

    feat(artist): implement artist component with signals for artist, albums, and tracks, plus computed grouping by record type

--- src/app/features/artist/artist.ts
+++ src/app/features/artist/artist.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Track, Artist as art, Album } from '../../core/models/searchModel';
+import { DeezerService } from '../../core/services/deezerService';
 
 @Component({
   selector: 'app-artist',
@@ -6,4 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './artist.html',
   styleUrl: './artist.scss',
 })
-export class Artist {}
+export class Artist {
+  //readonly id = input.required<string>();
+
+  readonly deezer = inject(DeezerService);
+
+  readonly artist = signal<art | null>(null);
+  readonly albums = signal<Album[]>([]);
+  readonly topTracks = signal<Track[]>([]);
+  readonly loading = signal(true);
+  readonly error = signal<string | null>(null);
+
+  readonly albumsByType = computed(() => {
+    const all = this.albums();
+    return {
+      album: all.filter((a) => a.record_type === 'album'),
+      single: all.filter((a) => a.record_type === 'single'),
+      ep: all.filter((a) => a.record_type === 'ep'),
+    };
+  });
+
+  readonly skeletonAlbums = Array.from({ length: 6 });
+  readonly skeletonTracks = Array.from({ length: 5 });
+}
