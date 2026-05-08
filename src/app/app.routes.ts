037ad64b7e1790262e5dd72d452eaabb0b6b4609
kemi-p <kpole@dvtsoftware.com>
2026-05-08 09:48:52 +0200

    feat(auth): guard routes to redirect unauthenticated useers to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { authGuardFn } from '@auth0/auth0-angular';
+
 import { albumResolver } from './core/resolvers/album.resolver';
 import { artistResolver } from './core/resolvers/artist.resolver';
 
@@ -6,41 +8,53 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./shared/layout/wrapper/wrapper').then((m) => m.Wrapper),
+
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
+
       {
         path: 'home',
         loadComponent: () => import('./features/home/home').then((m) => m.Home),
-        title: 'Home — Deezer',
       },
+
+      {
+        path: 'login',
+        loadComponent: () => import('./features/login/login').then((m) => m.Login),
+      },
+
       {
         path: 'search',
         loadComponent: () => import('./features/search/search').then((m) => m.Search),
-        title: 'Search — Deezer',
+        canActivate: [authGuardFn],
       },
+
       {
         path: 'artist/:id',
         loadComponent: () => import('./features/artist/artist').then((m) => m.Artist),
         resolve: { artistData: artistResolver },
-        title: 'Artist — Deezer',
+        canActivate: [authGuardFn],
       },
+
       {
         path: 'album/:id',
         loadComponent: () => import('./features/album/album').then((m) => m.Album),
         resolve: { albumData: albumResolver },
-        title: 'Album — Deezer',
+        canActivate: [authGuardFn],
       },
+
       {
         path: 'playlists',
         loadComponent: () => import('./features/playlist/playlist').then((m) => m.Playlist),
-        title: 'Playlists — Deezer',
+        canActivate: [authGuardFn],
       },
+
       {
         path: 'playlists/:id',
         loadComponent: () => import('./features/playlist/playlist').then((m) => m.Playlist),
-        title: 'Playlist — Deezer',
+        canActivate: [authGuardFn],
       },
     ],
   },
+
   { path: '**', redirectTo: 'home' },
 ];
